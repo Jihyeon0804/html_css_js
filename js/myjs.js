fce2223b77af136baf195bd317552744790527d0
Jihyeon0804 <cam080421@naver.com>
2025-07-04 16:49:39 +0900

    js

---
+++ js/myjs.js
@@ -0,0 +1,7 @@
+function rollbackF1() {
+            document.getElementById("f1").innerHTML="js 로드 순서1";
+        }
+
+function rollbackF2() {
+    document.getElementById("f2").innerHTML="js 로드 순서2";
+}
